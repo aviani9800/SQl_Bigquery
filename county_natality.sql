3b7d27f7b4aefbe19114bdab5f4830a97f0efd4c
Avinandan Dutta <75080175+aviani9800@users.noreply.github.com>
2022-03-01 01:03:59 +0530

    Update county_natality.sql

--- county_natality.sql
+++ county_natality.sql
@@ -6,7 +6,7 @@
 SELECT T1.Year,SUM (T1.Births) AS Total_Numbers_of_Birth
 FROM `bigquery-public-data.sdoh_cdc_wonder_natality.county_natality_by_father_race`as T1
 LEFT JOIN`bigquery-public-data.sdoh_cdc_wonder_natality.county_natality_by_mother_race`AS T2 
-ON T1.Ave_Birth_Weight_gms=T2.Ave_Birth_Weight_gms
+ON T1.County_of_Residence=T2.County_of_Residence
 WHERE Fathers_Single_Race ="Asian" AND Mothers_Single_Race="White" OR
 Fathers_Single_Race ="White" AND Mothers_Single_Race="Asian"
 GROUP BY 1
