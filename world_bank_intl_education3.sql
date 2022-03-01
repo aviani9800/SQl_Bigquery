c6921df56075b88cf5fea41a59c7ef88bfd4dae9
Avinandan Dutta <75080175+aviani9800@users.noreply.github.com>
2022-03-01 09:17:59 +0530

    Create world_bank_intl_education3.sql

---
+++ world_bank_intl_education3.sql
@@ -0,0 +1,12 @@
+--INTERNATIONAL EDUCATION 
+ 
+--world_bank_intl_education
+ 
+--Government of India spending on women's education between the periods 1990-2000. 
+ 
+SELECT year,country_name,value,short_definition AS Definition
+FROM `bigquery-public-data.world_bank_intl_education.international_education` as T1
+LEFT JOIN `bigquery-public-data.world_bank_intl_education.series_summary` as T2 ON T1.indicator_name=T2.indicator_name
+WHERE country_name='India'
+and year between  1990 and 2000
+and short_definition Like lower ('%female%')
